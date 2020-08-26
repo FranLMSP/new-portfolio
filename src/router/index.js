5491b113c2fbd16c0d9a55dd106688ff7b3982f2
Franco Colmenarez <francoacg1@gmail.com>
2020-08-25 21:52:44 -0500

    Creating the experience page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Knowledge from '../views/Knowledge.vue'
+import Experience from '../views/Experience.vue'
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: '/knowledge',
     name: 'Knowledge',
     component: Knowledge
+  },
+  {
+    path: '/experience',
+    name: 'Experience',
+    component: Experience
   }
 ]
 
---
+++ src/views/Experience.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="max-w-sm w-full max-w-full lg:flex">
+    <div class="w-full border bg-gray-100 lg:pl-10 border-gray-400 lg:border-gray-400 bg-white rounded p-4 flex flex-col justify-between leading-normal">
+      <div class="mb-8">
+        <h2 class="text-gray-900 font-bold text-5xl mb-2">Experience</h2>
+
+        <div class="max-w bg-white rounded overflow-hidden shadow-lg">
+          <div class="px-6 py-4">
+            <div class="font-bold text-center mb-0 text-xl">At work</div>
+            <svg class="m-auto mt-0" style="display: block;" width="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M320 336c0 8.84-7.16 16-16 16h-96c-8.84 0-16-7.16-16-16v-48H0v144c0 25.6 22.4 48 48 48h416c25.6 0 48-22.4 48-48V288H320v48zm144-208h-80V80c0-25.6-22.4-48-48-48H176c-25.6 0-48 22.4-48 48v48H48c-25.6 0-48 22.4-48 48v80h512v-80c0-25.6-22.4-48-48-48zm-144 0H192V96h128v32z"/></svg>
+
+            <hr class="m-6">
+
+            <div class="flex flex-wrap">
+              <div class="w-full lg:w-1/3 mb-6 lg:mb-0">
+                  <img style="width: 100%" src="img/dago.png" />
+              </div>
+
+              <div class="w-full lg:w-2/3">
+                <ul class="list-disc pl-8">
+                  <li class="mb-4">
+                    Five months working on-site at <strong>Importadora Dago C.A.</strong> at
+                    <i>San Antonio de Los Altos, Miranda, Venezuela</i> as
+                    <strong>Backend and Frontend developer</strong>, developing
+                    <strong>management systems</strong> and weight systems machines for
+                    other companies.
+                  </li>
+
+                  <li class="mb-4">
+                    Two years <strong>working remotely</strong> at <strong>Aleli</strong>,
+                    <i>Paraguay</i>, as <strong>Backend and Frontend developer</strong>,
+                    also developing <strong>management systems</strong> for inventory, clients,
+                    employees, incomings and outcomings, products, etc.
+                  </li>
+
+                  <li>
+                    Maintaining a few systems and databases.
+                  </li>
+                </ul>
+              </div>
+
+
+            </div>
+
+          </div>
+
+
+        </div>
+
+        <div class="max-w bg-white rounded overflow-hidden shadow-lg mt-5">
+          <div class="px-6 py-4">
+            <div class="font-bold text-center mb-0 text-xl">Academic</div>
+            <svg class="m-auto mt-0" style="display: block;" width="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M622.34 153.2L343.4 67.5c-15.2-4.67-31.6-4.67-46.79 0L17.66 153.2c-23.54 7.23-23.54 38.36 0 45.59l48.63 14.94c-10.67 13.19-17.23 29.28-17.88 46.9C38.78 266.15 32 276.11 32 288c0 10.78 5.68 19.85 13.86 25.65L20.33 428.53C18.11 438.52 25.71 448 35.94 448h56.11c10.24 0 17.84-9.48 15.62-19.47L82.14 313.65C90.32 307.85 96 298.78 96 288c0-11.57-6.47-21.25-15.66-26.87.76-15.02 8.44-28.3 20.69-36.72L296.6 284.5c9.06 2.78 26.44 6.25 46.79 0l278.95-85.7c23.55-7.24 23.55-38.36 0-45.6zM352.79 315.09c-28.53 8.76-52.84 3.92-65.59 0l-145.02-44.55L128 384c0 35.35 85.96 64 192 64s192-28.65 192-64l-14.18-113.47-145.03 44.56z"/></svg>
+
+            <hr class="m-6">
+
+            <div class="flex flex-wrap">
+
+              <div class="w-full lg:w-2/3">
+                <ul class="list-disc pl-8">
+                  <li class="mb-4">
+                    Graduated from the <i>Liceo San José</i> High School in the <strong>Data Proccessing (programming)</strong> mention.
+                  </li>
+
+                  <li class="mb-4">
+                    Graduated as <strong>University Superior Technician</strong> at the
+                    <i>Instituto Universitario de Tecnología - Federico Rivero Palacio (Distrito Capital)</i>
+                    college.
+                  </li>
+
+                  <li class="mb-4">
+                    Certifications at online courses like <strong>Platzi</strong> of Backend,
+                    Node.js, Docker, Regular Expresions, Javascript, Git and Github, Linux, Bash commands, etc.
+                  </li>
+
+                  <li>
+                    Constant self-taught education.
+                  </li>
+                </ul>
+              </div>
+
+              <div class="w-full lg:w-1/3 mb-6 lg:mb-0">
+                  <img style="width: 100%" src="img/iut.jpg" />
+                  <img style="width: 100%" src="img/platzi.png" />
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+        <div class="max-w bg-white rounded overflow-hidden shadow-lg mt-5">
+          <div class="px-6 py-4">
+            <div class="font-bold text-center mb-0 text-xl">Personal</div>
+            <svg class="m-auto mt-0" style="display: block;" width="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M384 144c0-44.2-35.8-80-80-80s-80 35.8-80 80c0 36.4 24.3 67.1 57.5 76.8-.6 16.1-4.2 28.5-11 36.9-15.4 19.2-49.3 22.4-85.2 25.7-28.2 2.6-57.4 5.4-81.3 16.9v-144c32.5-10.2 56-40.5 56-76.3 0-44.2-35.8-80-80-80S0 35.8 0 80c0 35.8 23.5 66.1 56 76.3v199.3C23.5 365.9 0 396.2 0 432c0 44.2 35.8 80 80 80s80-35.8 80-80c0-34-21.2-63.1-51.2-74.6 3.1-5.2 7.8-9.8 14.9-13.4 16.2-8.2 40.4-10.4 66.1-12.8 42.2-3.9 90-8.4 118.2-43.4 14-17.4 21.1-39.8 21.6-67.9 31.6-10.8 54.4-40.7 54.4-75.9zM80 64c8.8 0 16 7.2 16 16s-7.2 16-16 16-16-7.2-16-16 7.2-16 16-16zm0 384c-8.8 0-16-7.2-16-16s7.2-16 16-16 16 7.2 16 16-7.2 16-16 16zm224-320c8.8 0 16 7.2 16 16s-7.2 16-16 16-16-7.2-16-16 7.2-16 16-16z"/></svg>
+
+            <hr class="m-6">
+
+            <div class="flex flex-wrap">
+
+              <div class="w-full lg:w-1/3 mb-6 lg:mb-0">
+                  <img style="width: 100%" src="img/acmicpc.jpeg" />
+              </div>
+
+              <div class="w-full lg:w-2/3">
+
+                <ul class="list-disc pl-8">
+                  <li class="mb-4">
+                    <strong><a style="color: #039be5" href="https://icpc.baylor.edu/">ACM-ICPC 2016</a></strong> Coding marathon
+                    done at <strong>Universidad Central de Venezuela (UCV)</strong> or <strong>Central College of Venezuela</strong>.
+                  </li>
+
+                  <li class="mb-4">
+                    Developing game prototypes with <strong>C++</strong> & <strong>SDL</strong>
+                    and <strong>Javascript with Canvas</strong>.
+                  </li>
+
+                  <li class="mb-4">
+                    Projects at the college (CRUD's, games, etc).
+                  </li>
+
+                  <li class="mb-4">
+                    Actively helping to other developers with their issues at programming groups
+                  </li>
+                </ul>
+
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+
+
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'Experience',
+  components: {
+  }
+}
+
+</script>
