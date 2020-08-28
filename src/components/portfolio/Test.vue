90339ffb4280064a59c22af470a8ebbecc1c6f19
Franco Colmenarez <francoacg1@gmail.com>
2020-08-27 21:09:57 -0500

    Creating a modal component

---
+++ src/components/portfolio/Test.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p>Test component</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Test'
+}
+
+</script>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -4,10 +4,13 @@
       <div class="mb-8">
         <h2 class="text-gray-900 font-bold text-5xl mb-2">Portfolio</h2>
         <p class="mb-5 pl-5">
-          Here is a list of projects I have worked on.
+          Here is a list of some projects I have worked on.
           There are some projects I decided to not put in here for some reasons
-          (those are either not as important or very similar to the other ones),
-          so here are the most important ones for me.
+          (those are either not as important as I think, or very similar to the other ones,
+          or private projects, but you can find some of them on my
+          <a target="_blank" href="https://github.com/FranLMSP" style="color:#039be5;">Github</a>
+          or <a target="_blank" href="https://gitlab.com/FranLMSP" style="color:#039be5;" >Gitlab</a>),
+          so here are the most important ones for me. <br>
           Feel free to click on them to see more details!
         </p>
 
@@ -67,7 +70,9 @@
             </div>
         </div>
 
-
+        <modal :title="modalTitle" v-model:showModal="showModal" @modal-toggle="val => showModal = val">
+            <test></test>
+        </modal>
 
       </div>
     </div>
@@ -77,9 +82,27 @@
 
 <script>
 
+import Modal from '../components/Modal'
+import Test from '../components/portfolio/Test'
+
 export default {
   name: 'Experience',
+  data() {
+    return {
+      showModal: false,
+      portfolioName: 'none',
+      modalTitle: "Test title"
+    }
+  },
+  methods: {
+    selectPortfolio(title, name) {
+      this.modalTitle = title
+      this.portfolioName = name
+    }
+  },
   components: {
+    Modal,
+    Test,
   }
 }
 
