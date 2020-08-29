ec9c8e06aea2f0d4392000cc45de774d6248a6b3
Franco Colmenarez <francoacg1@gmail.com>
2020-08-29 14:47:18 -0500

    Closing the modal when clicking outside

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -4,12 +4,12 @@
         <div
           class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
         >
-          <div class="fixed inset-0 transition-opacity">
+          <div class="fixed inset-0 transition-opacity" @click="modalToggle(false)">
             <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
           </div>
           <span
             class="hidden sm:inline-block sm:align-middle sm:h-screen"
-          >&#8203;</span>
+          ></span>&#8203;
           <div
             class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg md:max-w-2xl lg:max-w-5xl sm:w-full"
             role="dialog"
