0f92a052d37fb91b4b1a678d9199f13bc332b805
Franco Colmenarez <francoacg1@gmail.com>
2020-10-21 13:30:54 -0500

    Build 404 file

--- vue.config.js
+++ vue.config.js
@@ -11,5 +11,31 @@ module.exports = {
           }
           return ret;
         })
+  },
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+    },
+    '404': {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: '404.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+    },
   }
 }
