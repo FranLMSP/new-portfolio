634f7ee16699090ed4a1ee6c486082737fd797dc
Franco Colmenarez <francoacg1@gmail.com>
2020-10-21 14:29:28 -0500

    Change main title

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,8 +9,8 @@
     </div>
     <div class="w-full border-r lg:pl-10 border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
       <div class="mb-8">
-        <h2 class="text-gray-900 font-bold text-5xl mb-2">Franco Colmenarez</h2>
-        <p class="text-gray-700 text-lg text-base mb-4">Software Developer as a job, programmer as a hobbie</p>
+        <h2 class="text-gray-900 font-bold text-5xl ">Software Developer</h2>
+        <p class="text-gray-700 text-3xl text-base mb-4">Franco Colmenarez</p>
         <table
           class="w-full mt-5 text-left text-md table-collapse"
         >
